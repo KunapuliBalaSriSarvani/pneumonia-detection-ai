5c4efcab53c732fbca11e4221cf19aba8f11a354
K. BALA SRI SARVANI <163731275+KunapuliBalaSriSarvani@users.noreply.github.com>
2026-02-13 23:55:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -183,4 +183,7 @@ def clear_history():
 
 # --- Run ---
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
